17322af0d5efdb8c72d16ed7996e1aa40254840e
ILYAS KERBAL <kerbalsc@gmail.com>
2022-07-28 12:35:49 +0100

    Dagger-hilt setup

--- app/src/main/java/io/github/ilyaskerbal/googleauthapp/MainActivity.kt
+++ app/src/main/java/io/github/ilyaskerbal/googleauthapp/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
 import io.github.ilyaskerbal.googleauthapp.navigation.SetupNavGraph
 import io.github.ilyaskerbal.googleauthapp.ui.theme.GoogleAuthAppTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/io/github/ilyaskerbal/googleauthapp/utils/Constants.kt
@@ -0,0 +1,7 @@
+package io.github.ilyaskerbal.googleauthapp.utils
+
+object Constants {
+
+    const val BASE_URL = "http://10.0.2.2:8080"
+
+}
