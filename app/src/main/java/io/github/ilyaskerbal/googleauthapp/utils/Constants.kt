f3d9cef8a1fb65955acb32cd952c3443de49a866
ILYAS KERBAL <kerbalsc@gmail.com>
2022-07-29 17:47:39 +0100

    One-tap sign in/up

--- app/src/main/java/io/github/ilyaskerbal/googleauthapp/utils/Constants.kt
+++ app/src/main/java/io/github/ilyaskerbal/googleauthapp/utils/Constants.kt
@@ -7,4 +7,7 @@ object Constants {
     const val PREFERENCES_SIGNED_IN_KEY = "signed_in_key"
 
     const val PREFERENCES_NAME = "preferences_name"
+
+    const val CLIENT_ID = "317133645044-v00i6ebjnfq8dbmgjg6iu9bq1kus197c.apps.googleusercontent.com"
+    // client id for the web app
 }
